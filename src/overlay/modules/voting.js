ca0e11a8b77f0402e438b03b2d1326f75972f8b3
Mariusz Dabrowski <wayrse@gmail.com>
2019-04-01 21:43:31 -0400

    Voting UI updated
    - Removed buy buttons for additional votes since they were removed from the game
    - Updated the UI hide icon to actually hide everything by adding `!important`

--- src/overlay/modules/voting.js
+++ src/overlay/modules/voting.js
@@ -6,69 +6,9 @@ function initVoting() {
   template.innerHTML = `
     <div class="voting-options__item">
       <div class="voting-button" data-command="!vote1">Vote 1</div>
-      <div class="additional-votes">
-        <div class="additional-votes__item" data-command="!vote1 buy4">
-          +4 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">1,000</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote1 buy3">
-          +3 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">750</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote1 buy2">
-          +2 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">500</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote1 buy1">
-          +1 extra vote
-          <span class="cost cost--bottom">
-            <div class="cost__price">250</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-      </div>
     </div>
     <div class="voting-options__item">
       <div class="voting-button" data-command="!vote2">Vote 2</div>
-      <div class="additional-votes">
-        <div class="additional-votes__item" data-command="!vote2 buy4">
-          +4 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">1,000</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote2 buy3">
-          +3 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">750</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote2 buy2">
-          +2 extra votes
-          <span class="cost cost--bottom">
-            <div class="cost__price">500</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-        <div class="additional-votes__item" data-command="!vote2 buy1">
-          +1 extra vote
-          <span class="cost cost--bottom">
-            <div class="cost__price">250</div>
-            <div class="cost__coin"></div>
-          </span>
-        </div>
-      </div>
     </div>
   `;
 
